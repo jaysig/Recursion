d2da71bce30aa5318b6db383ba20666b1033fa46
Jaysig <jasontsigmon@gmail.com>
2015-09-23 15:53:09 -0400

    Returns top level but not nested classes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
+//Default pulls an array of each instance of the class and all of its child elements
 var getElementsByClassName = function(className
 ){
-  // your code here
+  var result = [];
+  var classes = [];
+  var bodyE = document.body.childNodes;
+  //Get an array of classes
+  //For each Class pass in chconld nodes
+  //concat that to one array
+  //push that array into result.
+  for (var i =0;i<bodyE.length;i++){
+    if(bodyE[i].className !== undefined && bodyE[i].className !== ""){
+      classes.push(bodyE[i]);
+    }
+  }
+  classes.forEach(function(aClass){
+    if(className === aClass.className.split(' ')){
+      result.push(aClass);
+    }
+  });
+  return result;
+  //document.body
+  // document.body, element.childNodes, and element.classList
 };
+
+//Testing for First child did not work

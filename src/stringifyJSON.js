29cb0a9eed5b178144a8ce40005b8056bc41963a
Jaysig <jasontsigmon@gmail.com>
2015-09-24 13:32:17 -0400

    Finished JSONing arrays working on Objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,39 +2,76 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+var sjsT = function(obj){
+  return JSON.stringify(obj);
+};
 var stringifyJSON = function(obj) {
   result = "";
-  if (typeof obj === "string"){
-    result = obj.quote();
-  }
-  if (typeof obj !== "object" || obj === null){
-      if(typeof obj === "number" || obj === null || typeof obj === "boolean"){
-        result += obj + "";
-      } else if(typeof obj === "object" && Array.isArray(obj) === false){
+  if (typeof obj === "string") {
+    // result += obj + "";
+    result = '"'+obj+'"';
+    //fix string still
+  } else if (typeof obj !== "object" || obj === null) {
+        if (typeof obj === "number" || obj === null || typeof obj === "boolean") {
+          result += obj + "";
+        } else if (typeof obj === "object" && Array.isArray(obj) === false) {
 
-      } else if(Array.isArray(obj) === true){
-        obj = null;
-  } else if(typeof obj !== "object" && Array.isArray(element) === false){
-      obj.forEach(function(element){
-        if(typeof element === "number"){
-          result.push(element + ",");
-        } else if(element === null || typeof element === "boolean"){
-          result.push(element + ",");
-        } else if(typeof element === "object" && Array.isArray(element) === false){
-        }else if(Array.isArray(element) === true){
-          element = null;
-
-        }else if(typeof element === "object"){
-          element.forEach(function(key){
-            if (key !== "undefined" && typeof element[key] !== "function" ){
-              stringifyJSON(key);
-            }
-          });
-          result.push(element +"");
+        }
+    }
+    else if (Array.isArray(obj)) {
+      test = "";
+      obj.forEach(function(item,index){
+        if(Array.isArray(item)){
+          test += stringifyJSON(item);
+          //'['+item+']'
+        } else if (typeof item === "string"){
+          test += '"'+item+'"';
+        } else {
+          test += item;
+        }
+        if (index < obj.length -1){
+          test += ",";
         }
       });
+      result = '['+test+']';
+      // result = "[]";
+      //Covers Arrays
     }
-  return result;
-  }
+    else if (typeof obj === "object" && Array.isArray(obj) === false) {
+      //(typeof obj !== "object" && Array.isArray(element) === false)
+      //Traditional Objects
+      test = "";
+      for (var key in obj){
+        key = stringifyJSON(key);
+        oKey = stringifyJSON(obj[key]);
+        test += key + ":";
+        test += obj[key];
+        // if(Array.isArray(item)){
+        //   test += stringifyJSON(item);
+        //   //'['+item+']'
+        // } else if (typeof item === "string"){
+        //   test += '"'+item+'"';
+        // } else {
+        //   test += item;
+        // }
+        // if (index < obj.length -1){
+        //   test += ",";
+        // }
+      }
+      result = '{'+test+'}';
+      // obj.forEach(function(key) {
+      //  if (typeof element === "object" && Array.isArray(element) === false) {} else if (Array.isArray(element) === true) {
+      //     element = null;
+      //
+      //   } else if (typeof element === "object") {
+      //     element.forEach(function(key) {
+      //       if (key !== "undefined" && typeof element[key] !== "function") {
+      //         stringifyJSON(key);
+      //       }
+      //     });
+      //   }
+      // });
+    }
+    return result;
 };
+var sjsN = stringifyJSON;

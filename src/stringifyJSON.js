1dae23cd4191d65bef60e6fbb29bd36153a39278
Jaysig <jasontsigmon@gmail.com>
2015-09-24 14:32:28 -0400

    Removed bad code and unhelpful comments

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,14 +8,11 @@ var sjsT = function(obj){
 var stringifyJSON = function(obj) {
   result = "";
   if (typeof obj === "string") {
-    // result += obj + "";
     result = '"'+obj+'"';
-    //fix string still
   } else if (typeof obj !== "object" || obj === null) {
         if (typeof obj === "number" || obj === null || typeof obj === "boolean") {
           result += obj + "";
         } else if (typeof obj === "object" && Array.isArray(obj) === false) {
-
         }
     }
     else if (Array.isArray(obj)) {
@@ -73,31 +70,9 @@ var stringifyJSON = function(obj) {
           test += ",";
         }
         counter++;
-        // if(Array.isArray(item)){
-        //   test += stringifyJSON(item);
-        //   //'['+item+']'
-        // } else if (typeof item === "string"){
-        //   test += '"'+item+'"';
-        // } else {
-        //   test += item;
-        // }
-        // if (index < obj.length -1){
-        //   test += ",";
-        // }
+
       }
       result = '{'+test+'}';
-      // obj.forEach(function(key) {
-      //  if (typeof element === "object" && Array.isArray(element) === false) {} else if (Array.isArray(element) === true) {
-      //     element = null;
-      //
-      //   } else if (typeof element === "object") {
-      //     element.forEach(function(key) {
-      //       if (key !== "undefined" && typeof element[key] !== "function") {
-      //         stringifyJSON(key);
-      //       }
-      //     });
-      //   }
-      // });
     }
     return result;
 };
